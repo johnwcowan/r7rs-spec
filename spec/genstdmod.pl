6a40cec3261359e78a35b075de8300b80abae407
Alex Shinn <alexshinn@gmail.com>
2011-09-25 19:10:14 +0900

    adding --list option just to list identifiers in index

--- spec/genstdmod.pl
+++ spec/genstdmod.pl
@@ -8,33 +8,43 @@ use constant COLUMN_WIDTH => 16;
 
 my $list;
 
+my $list_only;
+if ($#ARGV >= 0 and $ARGV[0] =~ /^--?l(ist([-_]?only)?)?$/i) {
+  shift;
+  $list_only = 1;
+}
+
 while (<>) {
   if (/^\.(.*)/) {
     $list .= "$1\n";
   } else {
     if (length($list)) {
-      my $columnated;
-      my $column = 0;
-      my $space = "";
-      for my $id (sort split(/\s+/, $list)) {
-        my $new_column = $column + 1 + int(length($id) / COLUMN_WIDTH);
-        if ($new_column > COLUMNS) {
-          $columnated .= "\n";
-          $new_column -= $column;
-          $column = 0;
-          $space = "";
+      if ($list_only) {
+        print "$_\n" for sort split(/\s+/, $list);
+      } else {
+        my $columnated;
+        my $column = 0;
+        my $space = "";
+        for my $id (sort split(/\s+/, $list)) {
+          my $new_column = $column + 1 + int(length($id) / COLUMN_WIDTH);
+          if ($new_column > COLUMNS) {
+            $columnated .= "\n";
+            $new_column -= $column;
+            $column = 0;
+            $space = "";
+          }
+          $columnated .= $space . $id;
+          $space = " " x (($new_column-$column)*COLUMN_WIDTH - length($id));
+          $space .= " " x ($new_column-$column-1);
+          $column = $new_column;
         }
-        $columnated .= $space . $id;
-        $space = " " x (($new_column-$column)*COLUMN_WIDTH - length($id));
-        $space .= " " x ($new_column-$column-1);
-        $column = $new_column;
+        $columnated =~ s/_/\\_/g;
+        $columnated =~ s/\? /\?\\ /g;
+        $columnated =~ s/((?:\\.|\S)+)/{\\cf $1}/g;
+        print "$columnated\n";
+        undef $list;
       }
-      $columnated =~ s/_/\\_/g;
-      $columnated =~ s/\? /\?\\ /g;
-      $columnated =~ s/((?:\\.|\S)+)/{\\cf $1}/g;
-      print "$columnated\n";
-      undef $list;
     }
-    print;
+    print unless $list_only;
   }
 }
