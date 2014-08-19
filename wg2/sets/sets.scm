ddaa3f51fcf2521c9d9e787d821ab35baf3a5f32
John Cowan <cowan@ccil.org>
2014-08-19 16:56:30 -0400

    nearly final version of SRFI 113 and associated implementation

---
+++ wg2/sets/sets.scm
@@ -0,0 +1,42 @@
+(require-library srfi-69)
+(require-library comparators)
+(module sets ()
+  (import chicken scheme)
+  (import (except srfi-69 hash-table-for-each))
+  (import comparators)
+
+  (export set set-unfold)
+  (export set? set-contains? set-empty? set-disjoint?)
+  (export set-member set-comparator)
+  (export set-adjoin set-adjoin! set-replace set-replace!
+          set-delete set-delete! set-delete-all set-delete-all! set-search!)
+  (export set-size set-find set-count set-any? set-every?)
+  (export set-map set-for-each set-fold
+          set-filter set-remove set-remove set-partition
+          set-filter! set-remove! set-partition!)
+  (export set-copy set->list list->set list->set!)
+  (export set=? set<? set>? set<=? set>=?)
+  (export set-union set-intersection set-difference set-xor
+          set-union! set-intersection! set-difference! set-xor!)
+  
+  (export bag bag-unfold)
+  (export bag? bag-contains? bag-empty? bag-disjoint?)
+  (export bag-member bag-comparator)
+  (export bag-adjoin bag-adjoin! bag-replace bag-replace!
+          bag-delete bag-delete! bag-delete-all bag-delete-all! bag-search!)
+  (export bag-size bag-find bag-count bag-any? bag-every?)
+  (export bag-map bag-for-each bag-fold
+          bag-filter bag-remove bag-partition
+          bag-filter! bag-remove! bag-partition!)
+  (export bag-copy bag->list list->bag list->bag!)
+  (export bag=? bag<? bag>? bag<=? bag>=?)
+  (export bag-union bag-intersection bag-difference bag-xor
+          bag-union! bag-intersection! bag-difference! bag-xor!)
+  
+  
+  (export bag-unique-size bag-element-count bag-for-each-unique bag-fold-unique
+          bag-increment! bag-decrement! bag->set set->bag set->bag!
+          bag->alist alist->bag alist->bag!)
+
+  (include "sets-impl.scm")
+)
