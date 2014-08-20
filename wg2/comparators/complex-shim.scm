82cc722e7b8cd19714c7ba596163c3e997ac9baf
John Cowan <cowan@ccil.org>
2014-08-20 12:35:33 -0400

    Removing WG2-specific material from errata branch

--- wg2/comparators/complex-shim.scm
+++
@@ -1,9 +0,0 @@
-;;;; Dummy versions of real-part and imag-part
-
-;;; Include this file if your Scheme doesn't support real-part
-;;; and imag-part procedures.  Note that it is not a requirement to
-;;; actually support non-real numbers.
-
-(define (real-part z) z)
-
-(define (imag-part z) 0)
